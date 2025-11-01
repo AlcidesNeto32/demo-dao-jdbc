740e6ff7f2caaa6904532e69a71423f3b359d004
Alcides Neto <alcidesnda@gmail.com>
2025-10-31 23:39:30 -0300

    Creating project and git repository

---
+++ src/Db.java
@@ -0,0 +1,5 @@
+public class Db {
+     static final String URL = "jdbc:mysql://localhost:3306/coursejdbc";
+     static final String password = "NET.CoM123";
+     static final String user = "root";
+}
---
+++ src/db/DB.java
@@ -0,0 +1,57 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+public class DB {
+    static final String URL = "jdbc:mysql://localhost:3306/coursejdbc";
+    static final String PASSWORD = "NET.CoM123";
+    static final String USER = "root";
+    private static Connection conn = null;
+
+    public static Connection getConnection() {
+        if (conn == null) {
+            try {
+                conn = DriverManager.getConnection(URL,USER,PASSWORD);
+            } catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
+        return conn;
+    }
+
+    public static void closeConnection() {
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
+    }
+
+
+    public static void closeStatement(Statement st) {
+        if (st != null) {
+            try {
+                st.close();
+            } catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
+    }
+
+    public static void closeResultSet(ResultSet rs) {
+        if (rs != null) {
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
+    }
+}
