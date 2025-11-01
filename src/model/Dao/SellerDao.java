54aa5a9a66a8ba7ecaf855a58b93396b87712303
Alcides Neto <alcidesnda@gmail.com>
2025-11-01 00:12:54 -0300

    DepartmentDao and SellerDao Interfaces added

---
+++ src/model/Dao/SellerDao.java
@@ -0,0 +1,14 @@
+package model.Dao;
+
+import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.List;
+
+public interface SellerDao {
+    void insert (Seller obj);
+    void update (Seller obj);
+    void deleteById (Department obj);
+    Seller findById (Integer id);
+    List<Seller> findAll();
+}
