65d9aca6b3d41078d5b60fa0f0572ec0d87159d1
Alcides Neto <alcidesnda@gmail.com>
2025-10-31 23:51:53 -0300

    Department entity class added

--- src/Db.java
+++
@@ -1,5 +0,0 @@
-public class Db {
-     static final String URL = "jdbc:mysql://localhost:3306/coursejdbc";
-     static final String password = "NET.CoM123";
-     static final String user = "root";
-}
